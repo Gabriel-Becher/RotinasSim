320f2a1b05058e9c3a4c95ec59c39d953ab77df3
Gabriel A. Becher <gabriel.abecher@hotmail.com>
2025-11-06 18:16:50 -0300

    telas de cadastro e login, alternancia

--- app/src/main/java/com/dev/rotinassim/MainActivity.kt
+++ app/src/main/java/com/dev/rotinassim/MainActivity.kt
@@ -1,39 +1,49 @@
 package com.dev.rotinassim
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import com.dev.rotinassim.api.RetrofitInstance
 import com.dev.rotinassim.databinding.ActivityMainBinding
 import com.dev.rotinassim.room.DatabaseRoom
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-
-    lateinit var db: DatabaseRoom
+    private lateinit var bindind: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        bindind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindind.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        db = DatabaseRoom.getDatabase(this)
-
+        if(savedInstanceState == null){
+            mostraLogin()
+        }
     }
+
+    fun mostraLogin(){
+        supportFragmentManager.beginTransaction().replace(bindind.containerFragmento.id, LoginFragment()).commit()
+        bindind.botaoPrincipal.text = "Cadastrar-se"
+        bindind.botaoPrincipal.setOnClickListener {
+            mostraRegistro()
+        }
+    }
+
+    fun mostraRegistro(){
+        supportFragmentManager.beginTransaction().replace(bindind.containerFragmento.id, RegisterFragment()).commit()
+        bindind.botaoPrincipal.text = "Entrar"
+        bindind.botaoPrincipal.setOnClickListener {
+            mostraLogin()
+        }
+    }
+
 
 
 }
